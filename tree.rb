e02431c833a5fba87635a6e57d6f0ac9f0962c98
Jason Kucharski <jasonok@protonmail.ch>
2017-02-20 15:20:53 -0800

    add methods to Node

--- tree.rb
+++ tree.rb
@@ -1,10 +1,31 @@
 class Node
-  def initialize(value)
+  def initialize(value, left=nil, right=nil)
     @value = value
-    @children
+    @left = left
+    @right = right
   end
 
-  def add_child(children)
-    @children = children
+  def search(value) # search the tree for a certain value
+  end
+
+  def print_tree() # print out all nodes as they are visited in pre-order order
+  end
+
+  def preorder_search(start, find_val)
+    # """Helper method - use this to create a
+    # recursive search solution."""
+    return False
+  end
+
+  def preorder_print(start, traversal)
+    # """Helper method - use this to create a
+    # recursive print solution."""
+    return traversal
+  end
+end
+
+class BinaryTree
+  def initialize(root=nil)
+    @root = root
   end
 end
